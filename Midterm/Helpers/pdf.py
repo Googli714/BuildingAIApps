72cfe89ebeb87a85b91f99c72e0434e1c2609283
kiu_bedukadze.lazare <bedukadze.lazare@kiu.edu.ge>
2025-04-22 01:19:34 +0400

    Added documentation to main.py file, minor correction in retrieve_relevant_contexts method.

--- Midterm/Helpers/pdf.py
+++ Midterm/Helpers/pdf.py
@@ -4,8 +4,9 @@ import numpy as np
 from openai import OpenAI
 import PyPDF2
 
-from Midterm.Helpers.text import split_text_numpy
-from Midterm.sqlite_DB import VectorDB
+from BuildingAIApps.Midterm.Helpers.text import split_text_numpy
+from BuildingAIApps.Midterm.sqlite_DB import VectorDB
+
 
 def store_pdf_to_db(client: OpenAI, db: VectorDB, file_path: str):
     """
